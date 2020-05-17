f4733d530380cc80c9a068ac59b476793ac57bb8
Nikita Bakun <not-only-yours@users.noreply.github.com>
2020-05-17 13:01:08 +0300

    AST

--- Laba5/src/parseTree.java
+++ Laba5/src/parseTree.java
@@ -6,11 +6,13 @@ public class parseTree {
         Node right;
         Node left;
         String value;
+        Node parent;
 
-        public Node(Node left, Node right, String value) {
+        public Node(Node left, Node right, String value,Node parent) {
             this.left = left;
             this.right = right;
             this.value = value;
+            this.parent = parent;
         }
     }
 
@@ -23,7 +25,7 @@ public class parseTree {
     }
 
     public static void setFirst(String value) {
-        Node first = new Node(null, null, value);
+        Node first = new Node(null, null, value,null);
         current = first;
     }
 
@@ -34,26 +36,31 @@ public class parseTree {
         for (int i = 1; i < in.length-1; i++) {
             System.out.println(in[i]);
             if (current.left == null && !in[i].matches("[-+]?\\d+"))
-                current.left = new Node(null, null, in[i]);
+                current.left = new Node(null, null, in[i],current);
+
+
             if (current.left != null && !in[i].matches("[-+]?\\d+")) {
                 do {
                     current = current.left;
                 } while (current.left != null);
-                current.left = new Node(null, null, in[i]);
+                current.left = new Node(null, null, in[i],current);
             }
 
-            if(current.left == null && in[i].matches("[-+]?\\d+") && !in[i+1].matches("[-+]?\\d+")){
-              current = first;
-              if(current.left.right!=null) {
-                  do {
-                      current = current.left;
-                  } while (current.left.right != null);
-              }
-              current.right = new Node(null, null, in[i]);
+
+            if( current.left.value.matches("[-+]?\\d+") && current.right.value.matches("[-+]?\\d+")){
+                do{
+                    current=current.parent;
+                }while (current.right==null);
+                current.right=new Node(null, null, in[i],current);
             }
-            if(current.left != null && current.left != null&&in[i].matches("[-+]?\\d+")&& in[i+1].matches("[-+]?\\d+"))
-                current.right = new Node(null, null, in[i]);
-                current.left = new Node(null, null, in[i+1]);
+
+
+            if(current.value.matches("[-+]?\\d+")&&current.parent.left==null&&in[i].matches("[-+]?\\d+")){
+                current.parent.left = new Node(null, null, in[i],current);
+            }
+
+
+
         }
     }
 }
